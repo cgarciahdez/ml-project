1f37730a2bfedde37a37783e7a98b34e65195fdf
Jacob Kisiel <jkisiel@hawk.iit.edu>
2016-04-15 12:13:25 -0500

    Moved data loaders to their own file.

--- code/project.py
+++ code/project.py
@@ -9,7 +9,8 @@ from sklearn.utils import shuffle
 from sklearn.cross_validation import KFold
 from collections import defaultdict
 from collections import Counter
-
+from data_loaders import load_iris, load_vehicle, load_sonar, \
+			load_imgseg, load_bcw, load_pendig
 
 
 class Project():
@@ -32,17 +33,17 @@ class Project():
 			if var is 'h':
 				self.show_help()
 			elif var is '1':
-				self.load_iris()
+				self.x, self.y = load_iris()
 			elif var is '2':
-				self.load_vehicle()
+				self.x, self.y = load_vehicle()
 			elif var is '3':
-				self.load_sonar()
+				self.x, self.y = load_sonar()
 			elif var is '4':
-				self.load_imgseg()
+				self.x, self.y = load_imgseg()
 			elif var is '5':
-				self.load_bcw()
+				self.x, self.y = load_bcw()
 			elif var is '6':
-				self.load_pendig()
+				self.x, self.y = load_pendig()
 
 			print("Data has been loaded succesfully.")
 			c = input("Press 1 to choose a different data set or any other key to terminate: ")
@@ -55,133 +56,6 @@ class Project():
 	def show_help(self):
 		print("This program info info info")
 
-	def load_iris(self):
-		file = open("./data/iris.data.txt",'r').readlines()
-		labels={'Iris-setosa':0,'Iris-versicolor':1,'Iris-virginica':2}
-		x=[]
-		y=[]
-		for line in file:
-			l=line.rstrip()
-			l=l.split(',')
-			if(len(l)>1):
-			    xi=[]
-			    for i in range(0,len(l)-1):
-			        xi.append(float(l[i]))
-			    yi=labels[l[-1]]
-			    y.append(yi)
-			    x.append(xi)
-
-		x, y= np.array(x), np.array(y)
-		x, y = shuffle(x, y, random_state=0)
-		self.x, self.y= x,y
-
-
-	def load_vehicle(self):
-		letters = ['a','b','c','d','e','f','g','h','i']
-		x=[]
-		y=[]
-		for letter in letters:
-			file = open("./data/vehicle/xa%s.dat.txt"%letter,'r').readlines()
-			labels={'opel':0,'saab':1,'bus':2,'van':3}
-			for line in file:
-				l=line.rstrip()
-				l=l.split(' ')
-				if(len(l)>1):
-				    xi=[]
-				    for i in range(0,len(l)-1):
-				        xi.append(float(l[i]))
-				    yi=labels[l[-1]]
-				    y.append(yi)
-				    x.append(xi)
-
-		x, y= np.array(x), np.array(y)
-		x, y = shuffle(x, y, random_state=0)
-		self.x, self.y= x,y
-
-	def load_sonar(self):
-		file = open("./data/sonar.all-data.txt",'r').readlines()
-		labels={'R':0,'M':1}
-		x=[]
-		y=[]
-		for line in file:
-			l=line.rstrip()
-			l=l.split(',')
-			if(len(l)>1):
-			    xi=[]
-			    for i in range(0,len(l)-1):
-			        xi.append(float(l[i]))
-			    yi=labels[l[-1]]
-			    y.append(yi)
-			    x.append(xi)
-
-		x, y= np.array(x), np.array(y)
-		x, y = shuffle(x, y, random_state=0)
-		self.x, self.y= x,y
-
-	def load_imgseg(self):
-		file = open("./data/segmentation.data.txt",'r').readlines()
-		labels={'BRICKFACE':0, 'SKY':1, 'FOLIAGE':2, 'CEMENT':3, 'WINDOW':4, 'PATH':5, 'GRASS':6}
-		x=[]
-		y=[]
-		for line in file:
-			l=line.rstrip()
-			l=l.split(',')
-			if(len(l)>1):
-			    xi=[]
-			    for i in range(1,len(l)):
-			        xi.append(float(l[i]))
-			    yi=labels[l[0]]
-			    y.append(yi)
-			    x.append(xi)
-
-		x, y= np.array(x), np.array(y)
-		x, y = shuffle(x, y, random_state=0)
-		self.x, self.y= x,y
-
-	def load_bcw(self):
-		file = open("./data/breast-cancer-wisconsin.data.txt",'r').readlines()
-		labels={'2':0, '4':1}  #2 is benign, 4 is malign
-		x=[]
-		y=[]
-		for line in file:
-			l=line.rstrip()
-			l=l.split(',')
-			if(len(l)>1):
-				xi=[]
-				for i in range(1,len(l)-1):
-					if l[i] is '?':
-						l[i]='1'    #Possible change. Missing value replaced by 1, avg could be better choice.
-					xi.append(float(l[i]))
-				yi=labels[l[len(l)-1]]
-				y.append(yi)
-				x.append(xi)
-
-		x, y= np.array(x), np.array(y)
-		x, y = shuffle(x, y, random_state=0)
-		self.x, self.y= x,y
-
-	def load_pendig(self):
-		letters = ['tes','tra']
-		x=[]
-		y=[]
-		for letter in letters:
-			file = open("./data/pen-dig/pendigits.%s.txt"%letter,'r').readlines()
-			for line in file:
-				l="".join(line.split())
-				l=l.split(',')
-				if(len(l)>1):
-					xi=[]
-					for i in range(0,len(l)-1):
-						if(l[i] is '?'):
-							l[i]='1'    #Possible change. Missing value replaced by 1, avg could be better choice.
-						xi.append(int(l[i]))
-					yi=int(l[len(l)-1])
-					y.append(yi)
-					x.append(xi)
-
-		x, y= np.array(x), np.array(y)
-		x, y = shuffle(x, y, random_state=0)
-		self.x, self.y= x,y
 
 
 	def find_o(self):
@@ -301,9 +175,7 @@ class Project():
 
 proj = Project()
 #proj.main_menu()
-proj.load_iris()
+proj.x, proj.y = load_iris()
 print(len(proj.x))
 o= (proj.find_o())
 print(proj.classify(o,proj.x[76]))
-
-
